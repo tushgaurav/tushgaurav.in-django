31221f078249a3ffe02494968a1cc50516469e23
Tushar Gaurav <59288386+tushgaurav@users.noreply.github.com>
2023-01-05 19:09:39 +0530

    added login and registration feature

--- base/urls.py
+++ base/urls.py
@@ -4,8 +4,12 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name='home'),
+    path('login/', views.loginUser, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+    path('register/', views.register, name='register'),
     path("about/", views.about, name='about'),
     path("skills/", views.skills, name='skills'),
+    path("contact/", views.contact, name='contact'),
 
     path("<str:any>/", views.not_found, name='not-found')
 ]
--- base/views.py
+++ base/views.py
@@ -1,9 +1,16 @@
 import random
 
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponseNotFound
 from django.db.models import F
 from .models import Suggestion, Quote
+
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+from .forms import CreateUserForm
 
 def not_found(request, any):
     # Get a random quote for the 404 error screen
@@ -17,6 +24,47 @@ def not_found(request, any):
     }
     return render(request, 'base/not_found.html', context)
 
+def loginUser(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, "Username or password incorrect.")
+    context = {
+
+    }
+
+    return render(request, 'base/login.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, "Account created for " + username)
+        return redirect('login')
+    else:
+        form = CreateUserForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'base/register.html', context)
+
 def index(request):
     return render(request, 'base/homepage.html')
 
@@ -26,6 +74,11 @@ def about(request):
 def skills(request):
     return render(request, 'base/skills.html')
 
+@login_required(login_url='login')
+def contact(request):
+    return render()
+
+@login_required(login_url='login')
 def submit_suggestion(request):
     if request.method == 'POST':
 
