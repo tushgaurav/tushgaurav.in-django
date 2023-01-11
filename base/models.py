d520f1f7e6c085ddd877f503b524ae7e83675687
Tushar Gaurav <59288386+tushgaurav@users.noreply.github.com>
2023-01-11 17:51:07 +0530

    added Message model
    - message model stores the response from the contact page
    - updated Suggestion model with a new created field to store the time of
      creation and added meta class with ordering property
    - created and applied new migrations

--- base/models.py
+++ base/models.py
@@ -1,13 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Suggestion(models.Model):
     name = models.CharField(max_length=255)
     message = models.TextField()
     email = models.EmailField()
     mobile = models.CharField(max_length=10, default=models.SET_NULL)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name + "_" + self.message[:10]
+        
+    class Meta:
+        ordering = ['name', 'created']
 
 
 class Quote(models.Model):
@@ -16,3 +21,14 @@ class Quote(models.Model):
 
     def __str__(self):
         return self.category[:8].upper() + " " + self.text[:10]
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.first_name + " " + self.content[:10]
+
+    class Meta:
+        ordering = ['created']
