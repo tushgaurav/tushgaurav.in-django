688251ce0efc160d010f5499e9736f10c82f666d
Tushar Gaurav <59288386+tushgaurav@users.noreply.github.com>
2023-02-01 15:21:46 +0530

    updated about page

--- static/scripts/about.js
+++ static/scripts/about.js
@@ -3,5 +3,8 @@ import { Application } from "@splinetool/runtime";
 const canvas = document.getElementById("canvas3d");
 canvas.style.maxHeight = "50%";
 
+canvas.style.width = "100%";
+canvas.style.maxWidth = "1000px";
+
 const app = new Application(canvas);
 app.load("https://prod.spline.design/sPNFXvkM5t-R8qn5/scene.splinecode");
