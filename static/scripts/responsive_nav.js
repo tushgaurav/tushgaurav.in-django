c3244f327350f71f40dd8457cd3ea30833a11fc0
Tushar Gaurav <59288386+tushgaurav@users.noreply.github.com>
2023-03-11 06:47:17 +0530

    minor bug fixes

--- static/scripts/responsive_nav.js
+++ static/scripts/responsive_nav.js
@@ -2,6 +2,10 @@
 const menuIcon = document.getElementById("menu-icon");
 const nav = document.querySelector(".nav");
 
+if (window.innerWidth < 700) {
+  nav.style.display == "none";
+}
+
 menuIcon.addEventListener("click", () => {
   if (nav.style.display === "block") {
     nav.style.display = "none";
